3f40d9efb4da4d30dbefdbe175de88476b9e8d01
taylor <taylor@baremetalcloud.com>
2017-03-22 14:41:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ny-Times-App
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by taylor on 3/21/17.
+ */
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
